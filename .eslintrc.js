2e2019c186d0049a17ccefe869fb0ddf573d4a28
Brett Bloxom <brettbloxom@gmail.com>
2020-10-21 12:54:31 -0600

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,138 @@
+/* eslint-disable quotes */
+/* eslint-disable prettier/prettier */
+module.exports = {
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 2020, // Allows for the parsing of modern ECMAScript features
+    'sourceType': 'module', // Allows for the use of imports
+    'ecmaFeatures': {
+      'impliedStrict': true,
+      'classes': true,
+      'jsx': true
+    },
+  },
+  'env': {
+    'browser': true,
+    'node': true,
+    'jquery': true,
+    'jest': true,
+  },
+  'plugins': ['html', 'prettier', 'react-hooks', 'import'],
+  'extends': [
+    'airbnb',
+    'prettier', 
+    'prettier/react', 
+  ],
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'paths': ['.'],
+        'extensions': ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      'react': {
+        version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
+      }
+    },
+  },
+  'rules': {
+    'no-debugger': 0,
+    'no-alert': 0,
+    'no-await-in-loop': 0,
+    'no-return-assign': ['error', 'except-parens'],
+    'no-restricted-syntax': [2, 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+    'no-unused-vars': [
+      1,
+      {
+        'ignoreRestSiblings': true,
+        'argsIgnorePattern': 'res|next|^err',
+      },
+    ],
+    'prefer-const': [
+      'error',
+      {
+        'destructuring': 'all',
+      },
+    ],
+    'arrow-body-style': [2, 'as-needed'],
+    'no-unused-expressions': [
+      2,
+      {
+        'allowTaggedTemplates': true,
+      },
+    ],
+    'no-param-reassign': [
+      2,
+      {
+        'props': false,
+      },
+    ],
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    'import': 0,
+    'func-names': 0,
+    'space-before-function-paren': 0,
+    'comma-dangle': 0,
+    'max-len': 0,
+    'import/extensions': 0,
+    'no-underscore-dangle': 0,
+    'consistent-return': 0,
+    'react/display-name': 0,
+    'react/no-array-index-key': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/prefer-stateless-function': 0,
+    'react/forbid-prop-types': 0,
+    'react/no-unescaped-entities': 0,
+    'jsx-a11y/accessible-emoji': 0,
+    'react/require-default-props': 0,
+    'react/prop-types': 0,
+    'react/no-danger': 0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        'extensions': ['.tsx'],
+      },
+    ],
+    'react/jsx-props-no-spreading': [1, {
+      "exceptions":["Component", "svg", "Link"]
+    }],
+    'radix': 0,
+    'no-shadow': [
+      2,
+      {
+        'hoist': 'all',
+        'allow': ['resolve', 'reject', 'done', 'next', 'err', 'error'],
+      },
+    ],
+    'quotes': [
+      2,
+      'single',
+      {
+        'avoidEscape': true,
+        'allowTemplateLiterals': true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        'trailingComma': 'es5',
+        'singleQuote': true,
+        'jsxSingleQuote': true,
+        'semi': false,
+        'printWidth': 80,
+        'tabWidth': 2,
+      },
+    ],
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      {
+        'aspects': ['invalidHref'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'jsx-a11y/label-has-associated-control': [2, {
+      'controlComponents':['Field']
+    }]
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+const { screens } = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
+  theme: {
+    // Theme breakpoints - includes defaults plus additions.
+    screens: {
+      xs: '375px',
+      ...screens,
+      'until-xs': { max: '374px' },
+      'until-sm': { max: '639px' },
+      'until-md': { max: '767px' },
+      'until-lg': { max: '1023px' },
+      'until-xl': { max: '1279px' },
+    },
+    fontSize: {
+      h1: '8rem',
+      h2: '4.8rem',
+      h3: '3.6rem',
+      h4: '2.4rem',
+      h5: '2.2rem',
+      h6: '2rem',
+      base: '2rem',
+      sm: '1.6rem',
+      xs: '1.4rem',
+      xxs: '1rem',
+    },
+    extend: {
+      spacing: {
+        72: '18rem',
+        84: '21rem',
+        96: '24rem',
+      },
+    },
+  },
+
+  variants: {},
+  future: {
+    /**
+     * @see https://tailwindcss.com/docs/upcoming-changes
+     */
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+    defaultLineHeights: true,
+    standardFontWeights: true,
+  },
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    function ({ addUtilities, config }) {
+      addUtilities({
+        '.container': {
+          width: '100%',
+          maxWidth: config('theme.screens.lg'),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: config('theme.padding.8'),
+          paddingRight: config('theme.padding.8'),
+        },
+      })
+    },
+  ],
+}

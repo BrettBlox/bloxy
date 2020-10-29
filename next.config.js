8dec0d7edc1def993ff3b619b2bbe3c84b971df2
Brett Bloxom <brettbloxom@gmail.com>
2020-10-29 09:22:14 -0600

    removed next image

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   // Target must be serverless
   target: 'serverless',
-  images: {
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    iconSizes: [],
-    domains: ['bloxy.dev'],
-    path: '/',
-    loader: 'default',
-  },
 }

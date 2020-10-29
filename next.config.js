235b2a20e3472da32b1b8d15f5f87a489eca306c
Brett Bloxom <brettbloxom@gmail.com>
2020-10-29 09:14:14 -0600

    added images object to next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   // Target must be serverless
   target: 'serverless',
+  images: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    iconSizes: [],
+    domains: ['bloxy.dev'],
+    path: '/_next/image',
+    loader: 'default',
+  },
 }
